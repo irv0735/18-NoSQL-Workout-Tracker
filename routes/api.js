8fb7c3809f3668071706a3b57caf343567b68cb1
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-10-24 08:49:56 -0600

    Added additional API routes - not sure about the range route...seems the range is never defined by the front end

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-  .sort({ date: -1 })
+  .sort({ day: -1 })
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
@@ -13,9 +13,28 @@ router.get("/api/workouts", (req, res) => {
   });
 });
 
-
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
