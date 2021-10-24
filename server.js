c2a0c2181c8c8454e46e095af436ac5feb9cf14b
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-10-24 13:55:56 -0600

    testing some more route adjustments

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+require('./models');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
