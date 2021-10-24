40e173dd5dbc58b070d806c6491495305f81c18e
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-10-24 13:03:29 -0600

    updated index.js and server.js to reference ./Workout

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-require('./models');
+require('./models/Workout');
 
 const PORT = process.env.PORT || 3000;
 
