91986cf2ffcf708ae36d9202da4d994e84135306
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-10-24 11:28:18 -0600

    Total duration added via aggregate function

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-  .sort({ day: -1 })
-  .then(dbWorkout => {
-    res.json(dbWorkout);
+  Workout.aggregate([
+    { $match: {} }, 
+    { $addFields: { totalDuration: { $sum: '$exercises.duration' } } }
+  ])
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
   })
   .catch(err => {
     res.status(400).json(err);
@@ -33,7 +35,12 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  Workout.find({})
+  let range = new Date();
+  range.setDate(range.getDate()-7);
+  Workout.aggregate([
+    { $match: {day: {$gt: range}} }, 
+    { $addFields: { totalDuration: { $sum: '$exercises.duration' } } }
+  ])
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
